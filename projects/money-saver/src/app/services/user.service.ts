ce682b2b37ba936d3a52473895e5de5f227ab4dc
I.Denisovs <ide@adevo.io>
2020-09-14 17:28:21 +0300

    User API service is extending the abstract Stored service.

--- projects/money-saver/src/app/services/user.service.ts
+++ projects/money-saver/src/app/services/user.service.ts
@@ -1,40 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Store } from '@ngrx/store';
 import User from '../state/User';
 import { receivedUserAuthentication, runningUserAuthRequest } from '../state/user.actions';
-import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
-import { postMessage } from '../components/messages/messages.actions';
-import Message from '../components/messages/message';
+import { StoredService } from './stored.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
-  constructor(
-    private http: HttpClient,
-    private store: Store
-  ) {
-  }
-
+export class UserService extends StoredService {
   getAuth() {
     this.store.dispatch(runningUserAuthRequest());
 
     return this.http.get('/api/auth')
       .pipe(
-        catchError((error: HttpErrorResponse) => {
-          console.error(error.message);
-
-          this.store.dispatch(postMessage({
-            message: new Message('Something bad happens!')
-          }));
-
-          return of(null);
-        })
+        this.serverErrorHandler()
       )
-      .subscribe((response: User|null) => {
-        this.store.dispatch(receivedUserAuthentication({ user: response }));
+      .subscribe((user: User|null) => {
+        this.store.dispatch(receivedUserAuthentication({ user }));
       });
   }
 }
