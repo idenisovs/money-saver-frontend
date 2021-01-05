8b065d6cebd7420f8e49c97263185f4b57eb427a
Ilya Denisov <i.denisovs@gmail.com>
2021-01-05 13:12:07 +0200

    Display raw data.

--- projects/money-saver/src/app/views/dashboard/expenses-table/expenses-table.component.ts
+++ projects/money-saver/src/app/views/dashboard/expenses-table/expenses-table.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ScheduleItem } from '../../../shared';
 
 @Component({
   selector: 'app-expenses-table',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expenses-table.component.scss']
 })
 export class ExpensesTableComponent implements OnInit {
+
+  @Input()
+  schedule: ScheduleItem[];
 
   constructor() { }
 
--- projects/money-saver/src/app/views/dashboard/interval-overview/interval-overview.component.ts
+++ projects/money-saver/src/app/views/dashboard/interval-overview/interval-overview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Totals } from '../../../shared';
 
 @Component({
   selector: 'app-interval-overview',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interval-overview.component.scss']
 })
 export class IntervalOverviewComponent implements OnInit {
+
+  @Input()
+  totals: Totals;
 
   constructor() { }
 
