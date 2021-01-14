fde52d80292c53d1e8552238e9e5177bf4734793
Ilya Denisov <i.denisovs@gmail.com>
2021-01-14 20:16:44 +0200

    Added Intervals component and module.

---
+++ projects/money-saver/src/app/views/intervals/intervals.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IntervalsComponent } from './intervals.component';
+
+@NgModule({
+  declarations: [IntervalsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class IntervalsModule { }
