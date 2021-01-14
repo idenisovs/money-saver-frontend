5573a59a46cc573bf8bb0bffef7fc97f6fbc4909
Ilya Denisov <i.denisovs@gmail.com>
2021-01-14 20:16:34 +0200

    Added Years component and module.

---
+++ projects/money-saver/src/app/views/years/years.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { YearsComponent } from './years.component';
+
+
+
+@NgModule({
+  declarations: [YearsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class YearsModule { }
