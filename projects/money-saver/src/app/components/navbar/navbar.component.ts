9e699f839993f9af34aec0c047660b22161e1b9b
Ilya Denisov <i.denisovs@gmail.com>
2021-06-01 13:54:23 +0300

    Logout feature implemented in frontend.

--- projects/money-saver/src/app/components/navbar/navbar.component.ts
+++ projects/money-saver/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  collapsed = true;
+	collapsed = true;
+
+	constructor(
+		private http: HttpClient
+	) {
+	}
+
+	logout() {
+		this.http.get('/api/auth/logout').subscribe(() => {
+			window.location.reload();
+		});
+	}
 }
