b6eed4e4ed1d1a5714a959c27073aa7414ed6ec5
Ilya Denisov <i.denisovs@gmail.com>
2023-01-15 16:50:50 +0200

    Spinner might be shown with delay.

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent {
+export class SpinnerComponent implements OnInit {
 
+  @Input()
+  delay = 0;
+
+  isVisible = false;
+
+  ngOnInit() {
+    this.isVisible = this.delay === 0;
+
+    if (this.delay) {
+      setTimeout(() => {
+        this.isVisible = true;
+      }, this.delay);
+    }
+  }
 }
