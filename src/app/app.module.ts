71f6f7244352b0dfdce992379e7a75f7cc03810d
Ilya Denisov <i.denisovs@gmail.com>
2023-01-16 17:32:04 +0200

    Application got Reactive Forms support.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +18,7 @@ import { ComponentsModule } from './components/components.module';
 		BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
+    ReactiveFormsModule,
 		NgbModule,
 		LoginPageModule,
 		ComponentsModule
