992b0785ef2247219f00170d0db6cc7e3d4ca166
Ilja Denisovs <i.denisovs@gmail.com>
2023-01-30 18:22:24 +0200

    Requesting expenses summary from backend.

--- src/app/pages/expenses-page/expenses-page.component.ts
+++ src/app/pages/expenses-page/expenses-page.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Summary } from '../../shared';
+import { SummaryService } from '../../services/summary.service';
+import { LoadingPopupService } from '../../components/loading-popup/loading-popup.service';
+
 @Component({
   selector: 'app-expenses-page',
   templateUrl: './expenses-page.component.html',
   styleUrls: ['./expenses-page.component.scss']
 })
 export class ExpensesPageComponent implements OnInit {
-  ngOnInit() {
+  public summary: Summary|null = null;
+  public isSummaryLoading = false;
 
+  constructor(
+    private summaryService: SummaryService,
+    private loadingPopup: LoadingPopupService
+  ) {}
+  ngOnInit() {
+    this.requestExpensesSummary();
+  }
+
+  requestExpensesSummary() {
+    this.isSummaryLoading = true;
+
+    this.loadingPopup.open({
+      delay: 1000,
+      message: 'Requesting summary...'
+    });
+
+    this.summaryService.get().subscribe((summary: Summary|null) => {
+      this.loadingPopup.close();
+      this.isSummaryLoading = false;
+      this.summary = summary;
+    });
   }
 }
