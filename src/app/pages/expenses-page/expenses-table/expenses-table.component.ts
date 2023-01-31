9ff28c7db3c71d1d35475cdaa206ec4ee819624c
Ilja Denisovs <i.denisovs@gmail.com>
2023-01-31 15:59:22 +0200

    Expenses table got highlighting for past, current and future dates.

--- src/app/pages/expenses-page/expenses-table/expenses-table.component.ts
+++ src/app/pages/expenses-page/expenses-table/expenses-table.component.ts
@@ -10,4 +10,45 @@ import { Day } from '../../../shared';
 export class ExpensesTableComponent {
   @Input()
   expenses: Day[] = [];
+
+  getRowStyle(dailyExpensesOverview: Day) {
+    const today = this.getDateStr(new Date());
+    const expensesDate = this.getDateStr(dailyExpensesOverview.date);
+
+    if (expensesDate < today) {
+      return 'table-success';
+    }
+
+    if (expensesDate === today) {
+      return 'table-info';
+    }
+
+    return 'table-light';
+  }
+
+  public getDateStr(date = new Date()): string {
+    console.log(date.getDate());
+
+    const result: string[] = [];
+
+    result.push(date.getFullYear().toString());
+
+    let mm = (date.getMonth() + 1);
+
+    if (mm < 10) {
+      result.push('0' + mm);
+    } else {
+      result.push(mm.toString())
+    }
+
+    const dd = date.getDate();
+
+    if (dd < 10) {
+      result.push('0' + dd);
+    } else {
+      result.push(dd.toString())
+    }
+
+    return result.join('-');
+  }
 }
