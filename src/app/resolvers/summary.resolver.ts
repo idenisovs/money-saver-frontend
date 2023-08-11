bf128bc5a91ca2c049f31b992472c42711e2a77e
Ilja Denisovs <i.denisovs@gmail.com>
2023-08-11 12:47:50 +0300

    Angular Core updated.

--- src/app/resolvers/summary.resolver.ts
+++ src/app/resolvers/summary.resolver.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot
-} from '@angular/router';
+import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { Summary } from '../shared';
@@ -12,10 +8,10 @@ import { SummaryService } from '../services/summary.service';
 @Injectable({
   providedIn: 'root'
 })
-export class SummaryResolver implements Resolve<Summary> {
+export class SummaryResolver  {
   constructor(private summary: SummaryService) {
   }
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Summary> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Summary|null> {
     return this.summary.get();
   }
 }
