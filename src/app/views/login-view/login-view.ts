cac6de99042b40e26452c02262dca5be7f7821fd
Ilja Denisovs <i.denisovs@gmail.com>
2026-07-17 13:45:42 +0300

    Processing Login / Password input.

--- src/app/views/login-view/login-view.ts
+++ src/app/views/login-view/login-view.ts
@@ -1,10 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+type LoginForm = FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+}>;
 
 @Component({
   selector: 'app-login-view',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-view.html',
   styleUrl: './login-view.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginView {}
+export class LoginView {
+    private fb = inject(FormBuilder);
+
+    protected loginForm: LoginForm = this.fb.nonNullable.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+    });
+
+    protected login(): void {
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+        const credentials = this.loginForm.getRawValue();
+        // TODO: hook up authentication using `credentials`.
+        console.log(credentials)
+    }
+}
