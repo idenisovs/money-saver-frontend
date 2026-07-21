a25455072cfc5a594c360241e075de2d5522c9fd
Ilja Denisovs <i.denisovs@gmail.com>
2026-07-21 19:45:18 +0300

    Set the user once success.

--- src/app/guards/guest-guard.ts
+++ src/app/guards/guest-guard.ts
@@ -1,15 +1,18 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, UrlTree } from '@angular/router';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 import { AuthApi } from '@api/auth-api';
+import { UserService } from '@services/user-service';
 import { User } from '@shared';
 
 export const guestGuard: CanActivateFn = () => {
     const auth = inject(AuthApi);
     const router = inject(Router);
+    const userService = inject(UserService);
 
     return auth.getUser().pipe(
+        tap((user: User | null) => userService.setUser(user)),
         map((user: User | null): true | UrlTree => {
             if (user) {
                 return router.parseUrl('/dashboard')
